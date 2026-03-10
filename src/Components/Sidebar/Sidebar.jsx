7c79dd53648bc0ea55b44a63917ffc3c805d176a
lucasaalves629 <lucasaalves629@gmail.com>
2026-03-09 23:07:46 -0300

    Ajuste de navegação

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,29 +1,32 @@
+import { useNavigate } from 'react-router-dom';
 import './Sidebar.css';
+import { use } from 'react';
 
 
 export function Sidebar() {
+
+    const navigation = useNavigate();
 
     return(
         <div className='all-sidebar'>
 
 
 
-
-
+        {/* <img src="./public/Logo.png" className='logo-sidebar'/> */}
 
 
         {/* boxs */}
 
         <div className='wall-sidebar'></div>
-
+        
 
         {/* buttons */}
         <div className='bt-sidebar'>
-            <button type='button' className='b-sidebar' >Início</button>
-            <button type='button' className='b-sidebar' >Solicitações</button>
-            <button type='button'className='b-sidebar' >Serviços</button>
-            <button type='button'className='b-sidebar' >Perfil</button>
-            <button type='button'className='b-sidebar' >Sair</button>
+            <button type='button' onClick={() => navigation("/home")} className='b-sidebar' >Início</button>
+            <button type='button' onClick={() => navigation("")} className='b-sidebar' >Solicitações</button>
+            <button type='button' onClick={() => navigation("")} className='b-sidebar' >Serviços</button>
+            <button type='button' onClick={() => navigation("/perfil")} className='b-sidebar' >Perfil</button>
+            <button type='button' onClick={() => navigation("/login")} className='b-sidebar' >Sair</button>
         </div>
 
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,22 @@
+import { Sidebar } from '../../Components/Sidebar/Sidebar'
 import './Home.css'
+
+
 
 export function Home(){
 
     return(
-        <div>
-            <h1 className='t1home'>Bom dia</h1>
+        <div className='all-home'>
+
+            
+            {/* <img src="./public/Logo.png" className='logo-home'/> */}
+
+
+        {/* component */}
+
+        <Sidebar />
+
+
         </div>
     )
 }
